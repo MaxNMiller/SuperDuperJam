bf4b31cd2c7ac25a117705171abe2a15ba551b1e
liamcwang <157174486+liamcwang@users.noreply.github.com>
2024-04-26 20:36:43 -0400

    uiscripts

--- Assets/Scripts/CanvasController.cs
+++ Assets/Scripts/CanvasController.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 
 public class CanvasController : MonoBehaviour
 {
+    CanvasView view;
+
     // Start is called before the first frame update
     void Start()
     {
         GameManager.canvasController = this;
+        view = GetComponent<CanvasView>();
+        if (view == null) {
+            Debug.LogError($"I expected a view attached");
+        }
     }
 
     // Update is called once per frame
